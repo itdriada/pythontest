30852a8e539b0c634a7f3ffd668ac4b3ff581040
Alevtina Gridchina <alevtina.gridchina@yandex.ru>
2022-11-26 12:44:59 +0300

    Files

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from vklad import SuperVklad, CapitalVklad
 
 if __name__ == '__main__':
 
-    ivan = Client('Иван Петров', 500000, 8, 12)
+    ivan = Client('Соловей Петров', 500000, 8, 12)
     elena = SuperVklad('Светлана Петрова', 700000, 9, 12)
-    ekaterina = SuperVklad('Екатерна Долгова', 550000, 8, 16)
-    sergey = CapitalVklad('Сергей Иванов', 690000, 12, 14)
+    ekaterina = SuperVklad('Елена Долгова', 550000, 8, 16)
+    sergey = CapitalVklad('Кирилл Иванов', 690000, 12, 14)
 
     print(ivan)
     print(ivan.vklad(), '\n')
